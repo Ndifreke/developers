365ee5cf1db8252680bc8f672c4d741131b9b308
Ndifreke Ekim <ndifreke.ekim@andela.com>
2019-04-29 13:38:56 +0100

    fix(dialog-layout): make share dialog design consistent with mockup

--- app/src/main/java/com/ndifreke/developers/dialog/ProfileShareDialog.java
+++ app/src/main/java/com/ndifreke/developers/dialog/ProfileShareDialog.java
@@ -4,46 +4,41 @@ import com.ndifreke.developers.model.GithubUser;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-import android.util.Log;
+import android.support.v7.app.AppCompatDialogFragment;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import com.ndifreke.developers.R;
 
 
-public class ProfileShareDialog extends DialogFragment {
+public class ProfileShareDialog extends AppCompatDialogFragment {
 
-    public static AlertDialog.Builder builder;
+    public AlertDialog.Builder dialog;
+    public GithubUser githubUser;
+
 
     public ProfileShareDialog(){
     }
 
-    public static ProfileShareDialog newInstance(GithubUser githubUser, Context context){
-        return new ProfileShareDialog();
+    public ProfileShareDialog(GithubUser githubUser){
+        super();
+        this.githubUser = githubUser;
     }
 
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstance) {
-        builder = new AlertDialog.Builder(getActivity());
-        View view = getActivity().getLayoutInflater().inflate(R.layout.profile_share_dialog, null);
-        builder.setView(view);
-        LinearLayout dialogLayout = (LinearLayout) view;
-        TextView gihubName = dialogLayout.findViewById(R.id.share_github_name);
+        dialog = new AlertDialog.Builder(getActivity());
+        View dialogLayout = getActivity().getLayoutInflater().inflate(R.layout.profile_share_dialog, null);
+        TextView githubName = dialogLayout.findViewById(R.id.share_github_name);
         TextView githubURL = dialogLayout.findViewById(R.id.share_github_url);
-
-        OnClickListener onClose = new OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-            }
-        };
-
-        builder.setPositiveButton("Positive", onClose );
-        Log.i("dialog", builder.toString());
-        return builder.create();
+        final TextView closeDialog = dialogLayout.findViewById(R.id.closeDialog);
+        githubName.setText("@".concat(this.githubUser.getName()));
+        githubURL.setText(this.githubUser.getProfileURL());
+        dialog.setView(dialogLayout);
+        return dialog.create();
     };
 };
